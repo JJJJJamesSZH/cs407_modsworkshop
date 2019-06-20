61be5ad3c90fbc097f2537ab904f2f184f47e237
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-06-19 21:47:30 -0400

    Database file initialize and database manipulation initialize

---
+++ application/model/entity/user_file.js
@@ -0,0 +1,28 @@
+var Sequelize = require('sequelize');
+var sequelize = require('../db');
+
+const user_file = sequelize.define('user_file', {
+    uid: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    username: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    fileList: {
+        type: Sequelize.STRING
+    }
+})
+
+exports.user_file = user_file;
---
+++ application/model/files.js
@@ -0,0 +1,9 @@
+import { files } from "./entity/files";
+
+exports.addFile = async function(content) {
+
+}
+
+exports.deleteFile = async function(content) {
+
+}
