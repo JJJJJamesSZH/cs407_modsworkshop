1c5afd36e9ec3c4279291a558da8a4cfce50b1b0
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-06-20 22:52:10 -0400

    Update user_login.js

--- application/model/user_login.js
+++ application/model/user_login.js
@@ -111,7 +111,7 @@ exports.checkPassword = async function(content) {
     if (comp_result) {
         console.log("comp_result: ", comp_result);
         // generate new token
-        let token = jwt.sign({ email: email }, jwt_key, { algorithm: 'RS256' });
+        let token = jwt.sign({ email: email }, jwt_key);
         let result = {
             "status": 200,
             "token": token
