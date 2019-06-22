f0492c82d41007daf0099d0eefc729cc3091e9ca
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-06-22 01:11:13 -0400

    Add authorization check
    Add authorization check to all profile manipulation

--- application/profiles/profile_management.js
+++ application/profiles/profile_management.js
@@ -27,48 +27,96 @@ exports.getProfile = async(ctx, next) => {
 
 exports.getUsername = async(ctx, next) => {
     let body = ctx.request.body;
+    let verified = await jwtChecker.decodeAuth(ctx);
 
-    let controller = new Controller();
-    let result = await controller.getUsername(body);
-    ctx.body = result;
+    if (verified === false){
+        let result = {
+            "status": 500,
+            "err_message": "authorization code invalid"
+        }
+        console.log("authorization code invalid");
+        ctx.body = result;
+        await next();
+    }
+    else{
+        let controller = new Controller();
+        let result = await controller.getUsername(body);
+        ctx.body = result;
 
-    console.log("profile_management.result: ", result);
+        console.log("profile_management.result: ", result);
 
-    await next();
+        await next();
+    }
 }
 
 exports.getDescription = async(ctx, next) => {
     let body = ctx.request.body;
+    let verified = await jwtChecker.decodeAuth(ctx);
 
-    let controller = new Controller();
-    let result = await controller.getDescription(body);
-    ctx.body = result;
+    if (verified === false){
+        let result = {
+            "status": 500,
+            "err_message": "authorization code invalid"
+        }
+        console.log("authorization code invalid");
+        ctx.body = result;
+        await next();
+    }
+    else{
+        let controller = new Controller();
+        let result = await controller.getDescription(body);
+        ctx.body = result;
 
-    console.log("profile_management.result: ", result);
+        console.log("profile_management.result: ", result);
 
-    await next();
+        await next();
+    }
 }
 
 exports.getIcon = async(ctx, next) => {
     let body = ctx.request.body;
+    let verified = await jwtChecker.decodeAuth(ctx);
 
-    let controller = new Controller();
-    let result = await controller.getIcon(body);
-    ctx.body = result;
+    if (verified === false){
+        let result = {
+            "status": 500,
+            "err_message": "authorization code invalid"
+        }
+        console.log("authorization code invalid");
+        ctx.body = result;
+        await next();
+    }
+    else {
+        let controller = new Controller();
+        let result = await controller.getIcon(body);
+        ctx.body = result;
 
-    console.log("profile_management.result: ", result);
+        console.log("profile_management.result: ", result);
 
-    await next();
+        await next();
+    }
 }
 
 exports.editProfile = async(ctx, next) => {
     let body = ctx.request.body;
+    let verified = await jwtChecker.decodeAuth(ctx);
 
-    let controller = new Controller();
-    let result = await controller.editProfile(body);
-    ctx.body = result;
+    if (verified === false){
+        let result = {
+            "status": 500,
+            "err_message": "authorization code invalid"
+        }
+        console.log("authorization code invalid");
+        ctx.body = result;
+        await next();
+    }
+    else {
+        let controller = new Controller();
+        let result = await controller.editProfile(body);
+        ctx.body = result;
 
-    console.log("profile_management.result: ", result);
+        console.log("profile_management.result: ", result);
 
-    await next();
+        await next();
+    }
 }
