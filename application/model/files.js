8e44061972dd950c531d1ac8288d1f42de1c94c9
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-06-24 21:23:28 -0400

    Fixed async problem
    using Promise

--- application/model/files.js
+++ application/model/files.js
@@ -15,20 +15,25 @@ AWS.config.update({
 let s3 = new AWS.S3({ apiVersion: s3_config.apiVersion });
 
 exports.listFiles = async function(content) {
-    if (content.email === undefined) {
-        // do nothing, list all files
-    } else {
-        params["Prefix"] = content.email;
-        console.log("prefix = ", content.email);
-    }
-    s3.listObjectsV2(params, function(err, data) {
-        if (err) {
-            console.log(err, err.stack);
-        } // an error occurred
-        else {
-            console.log(data);
-        } // successful response
-    });
+    return new Promise(function(resolve, reject) {
+        if (content.email === undefined) {
+            // do nothing, list all files
+        } else {
+            params["Prefix"] = content.email;
+            console.log("prefix = ", content.email);
+        }
+        s3.listObjectsV2(params, function(err, data) {
+            if (err) {
+                // console.log(err, err.stack);
+                reject(err.stack);
+            } // an error occurred
+            else {
+                console.log(data);
+                // return data.Contents;
+                resolve(data.Contents);
+            } // successful response
+        });
+    })
 }
 
 exports.addFile = async function(content) {
