8adea28426bb2439217f770ff67189c2ec75d964
Tong An <an82@purdue.edu>
2019-06-24 20:09:43 -0400

    view/edit username, description, icon (seperated) functions

--- application/profiles/profile_management.js
+++ application/profiles/profile_management.js
@@ -108,3 +108,76 @@ exports.editProfile = async(ctx, next) => {
         await next();
     }
 }
+
+exports.editIcon = async(ctx, next) => {
+    let body = ctx.request.body;
+    let verified = await jwtChecker.decodeAuth(ctx);
+
+    if (verified !== body.email){
+        let result = {
+            "status": 500,
+            "err_message": "authorization code invalid"
+        }
+        console.log("authorization code invalid");
+        ctx.body = result;
+        await next();
+    }
+    else {
+        let controller = new Controller();
+        let result = await controller.editIcon(body);
+        ctx.body = result;
+
+        console.log("profile_management.result: ", result);
+
+        await next();
+    }
+}
+
+exports.editDescription = async(ctx, next) => {
+    let body = ctx.request.body;
+    let verified = await jwtChecker.decodeAuth(ctx);
+
+    if (verified !== body.email){
+        let result = {
+            "status": 500,
+            "err_message": "authorization code invalid"
+        }
+        console.log("authorization code invalid");
+        ctx.body = result;
+        await next();
+    }
+    else {
+        let controller = new Controller();
+        let result = await controller.editDescription(body);
+        ctx.body = result;
+
+        console.log("profile_management.result: ", result);
+
+        await next();
+    }
+}
+
+
+exports.editUsername = async(ctx, next) => {
+    let body = ctx.request.body;
+    let verified = await jwtChecker.decodeAuth(ctx);
+
+    if (verified !== body.email){
+        let result = {
+            "status": 500,
+            "err_message": "authorization code invalid"
+        }
+        console.log("authorization code invalid");
+        ctx.body = result;
+        await next();
+    }
+    else {
+        let controller = new Controller();
+        let result = await controller.editUsername(body);
+        ctx.body = result;
+
+        console.log("profile_management.result: ", result);
+
+        await next();
+    }
+}
--- routes/profile_view.js
+++ routes/profile_view.js
@@ -2,5 +2,9 @@ const router = require('koa-router')();
 const profile_mgmt = require("../application/profiles/profile_management");
 
 router.post('/modsworkshop/profile/viewAll', profile_mgmt.getProfile);
+router.post('/modsworkshop/profile/viewIcon', profile_mgmt.getIcon);
+router.post('/modsworkshop/profile/viewDescription', profile_mgmt.getDescription);
+router.post('/modsworkshop/profile/viewUsername', profile_mgmt.getUsername);
+
 
 module.exports = router;
