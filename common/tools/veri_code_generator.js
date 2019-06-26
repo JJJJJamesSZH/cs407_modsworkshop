baa090011fa1104eda69c02bbb900492e09c9dff
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-06-25 23:46:03 -0400

    Fixed the error caused by round.

--- common/tools/veri_code_generator.js
+++ common/tools/veri_code_generator.js
@@ -3,7 +3,7 @@ module.exports = function(n) {
 
     var i = 0;
     for (i = 0; i < 8; i++) {
-        var n = Math.round(Math.random() * 36);
+        var n = Math.floor(Math.random() * 36);
         if (n < 26) {
             var c_int = "A".charCodeAt(0) + n;
             var c = String.fromCharCode(c_int);
