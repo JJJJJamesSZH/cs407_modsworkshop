37a4569e7d6aa8eb946a96139701d4df07baba21
Ziheng Shao <jjjjjames.szh@gmail.com>
2019-07-20 00:47:50 -0400

    unit test rate files

--- test/rate_file.test.js
+++ test/rate_file.test.js
@@ -6,27 +6,70 @@ let assert = require("assert");
 
 let auth = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoYW80NEBwdXJkdWUuZWR1IiwiaWF0IjoxNTYxNDMzMzM2fQ.OiYdyHEMFzMBTBUpCkxev8_sbuUW9vsl9JqJqLyhty0"
 
-// testing: get the rate in listAll
+// testing: get the rate in file listAll
 it("testing: get rate in list all", function(done) {
     this.timeout(3000);
     setTimeout(function() {
         let test_case = {}
         request(server)
-        .post('/modsworkshop/file/listAll')
-        .send(test_case)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .expect(function(res) {
-            let body = res.body;
-            let file_list = body.file_list;
-            let n = file_list.length;
-            for (let i = 0; i < n; i++){
-                let rate = file_list[i].rate;
+            .post('/modsworkshop/file/listAll')
+            .send(test_case)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(function(res) {
+                let body = res.body;
+                let file_list = body.file_list;
+                let n = file_list.length;
+                for (let i = 0; i < n; i++){
+                    let rate = file_list[i].rate;
+                    assert.notEqual(rate, undefined);
+                    assert.notEqual(rate, null);
+                }
+            })
+            .end(done)
+    }, 10, 'funky');
+})
+
+// testing: get the rate in fileDetail
+it("testing: get rate in fileDetail", function(done) {
+    this.timeout(3000);
+    setTimeout(function() {
+        let test_case = {
+            key: "shao44@purdue.edu|testfile1.txt"
+        }
+        request(server)
+            .post('/modsworkshop/file/fileDetail')
+            .send(test_case)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(function(res) {
+                let body = res.body;
+                let rate = body.rate;
                 assert.notEqual(rate, undefined);
                 assert.notEqual(rate, null);
-            }
-        })
-        .end(done)
-    }, 10, 'funky');
+            })
+            .end(done)
+    }, 20, 'funky');
+})
+
+// testing: rate the file
+it("testing: rate the file", function(done) {
+    this.timeout(3000);
+    setTimeout(function() {
+        let test_case = {
+            "email":"shao44@purdue.edu",
+	        "key":"shao44@purdue.edu|testfile1.txt",
+	        "rate":5
+        }
+        request(server)
+            .post('/modsworkshop/rates/rateFile')
+            .send(test_case)
+            .set('Authorization', auth)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(done)
+    }, 30, 'funky');
 })
